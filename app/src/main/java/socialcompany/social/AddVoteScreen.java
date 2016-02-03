36a270583083cfe8e2e0ba4b96766d168d25765b
Signo <evanmorgan31@gmail.com>
2016-02-03 19:32:31 +0000

    Home Screen and Add Vote

---
+++ app/src/main/java/socialcompany/social/AddVoteScreen.java
@@ -0,0 +1,34 @@
+package socialcompany.social;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class AddVoteScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_vote_screen);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*0.8),(int)(height*0.8));
+
+        Button SubmitVoteButton = (Button) findViewById(R.id.button);
+        SubmitVoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View u) {
+                startActivity(new Intent(AddVoteScreen.this, HomeScreen.class));
+            }
+        });
+    }
+}
--- app/src/main/java/socialcompany/social/HomeScreen.java
+++ app/src/main/java/socialcompany/social/HomeScreen.java
@@ -1,13 +1,28 @@
 package socialcompany.social;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        Button AddVoteButton = (Button) findViewById(R.id.button);
+        AddVoteButton.setOnClickListener(new View.OnClickListener()
+        {
+           @Override
+            public void onClick(View v)
+           {
+               startActivity(new Intent(HomeScreen.this,AddVoteScreen.class));
+           }
+        });
     }
-}
+};
