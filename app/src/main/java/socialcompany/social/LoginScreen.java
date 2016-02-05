fd9c1f3c2a08d0f42d07f7e7b2fa3f0ab3ad0ac7
Simon Fitzgerald <fitzges5@tcd.ie>
2016-02-05 14:40:37 +0000

    Added Login/Create Activity

---
+++ app/src/main/java/socialcompany/social/LoginScreen.java
@@ -0,0 +1,33 @@
+package socialcompany.social;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+
+        Button CreateAccountButton = (Button) findViewById(R.id.CreateAccountButton);
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginScreen.this, HomeScreen.class));
+            }
+        });
+
+        Button LoginButton = (Button) findViewById(R.id.LoginButton);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginScreen.this, HomeScreen.class));
+            }
+        });
+    }
+}
